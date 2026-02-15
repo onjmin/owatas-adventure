d7d1418a3727b4a487391e5ac4269dcc9fe994be
おんJ民 <J onjmin@users.noreply.github.com>
2026-02-15 23:40:47 +0900

    Revert "fix"
    This reverts commit 410f6efb0d81945f7eac55585f1ddabac00384fb.

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ import { join } from 'path';
 const app = new Hono();
 
 app.get('/', () => {
-  return new Response(readFileSync(join('./docs', 'index.html')), {
+  return new Response(readFileSync(join('./public', 'index.html')), {
     headers: { 'Content-Type': 'text/html' },
   });
 });
 
 app.use('/src/*', serveStatic({ root: './' }));
 
-app.use('/*', serveStatic({ root: './docs/' }));
+app.use('/*', serveStatic({ root: './public/' }));
 
 export default {
   port: 8080,
